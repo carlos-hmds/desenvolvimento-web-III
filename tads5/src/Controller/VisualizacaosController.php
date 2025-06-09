69865c23f380abea37086329399649ce9448fc02
carlos-hmds <151768448+carlos-hmds@users.noreply.github.com>
2025-06-08 22:23:21 -0400

    Função viewManutencao() finalizada.

--- tads5/src/Controller/VisualizacaosController.php
+++ tads5/src/Controller/VisualizacaosController.php
@@ -1,9 +1,7 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Controller;
-
-use App\Controller\AppController;
-use Cake\Event\EventInterface;
 
 class VisualizacaosController extends AppController
 {
@@ -12,50 +10,49 @@ class VisualizacaosController extends AppController
         $response = null;
         $statusCode = 200;
 
-        if (!$this->request->is("POST"))
-        {
-            return $this->gerarResposta(400, "Requisição inválida.");
+        if (!$this->request->is('POST')) {
+            return $this->gerarResposta(400, 'Requisição inválida.');
         }
 
         debug($this->request->getData());
 
-        if (empty($this->request->getData()))
-        {
+        if (empty($this->request->getData())) {
             $manutencaos = $this->fetchTable('Manutencaos');
             $response = $manutencaos
                 ->find()
                 ->select([
-                    "cnpj_fornecedor" => "Fornecedors.cnpj",
-                    "nome_fornecedor" => "Fornecedors.nome",
-                    "modelo_veiculo" => "Veiculos.modelo",
-                    "placa_veiculo" => "Veiculos.placa",
+                    'cnpj_fornecedor' => 'Fornecedors.cnpj',
+                    'nome_fornecedor' => 'Fornecedors.nome',
+                    'modelo_veiculo' => 'Veiculos.modelo',
+                    'placa_veiculo' => 'Veiculos.placa',
+                    'nome_fabricante' => 'Fabricantes.nome',
                 ])
                 ->join([
-                    "table" => "Fornecedors",
-                    "type" => "INNER",
-                    "conditions" => "Fornecedors.id = Manutencaos.fornecedor_id"
+                    'table' => 'Fornecedors',
+                    'type' => 'INNER',
+                    'conditions' => 'Fornecedors.id = Manutencaos.fornecedor_id',
                 ])
                 ->join([
-                    "table" => "Veiculos",
-                    "type" => "INNER",
-                    "conditions" => "Veiculos.id = Manutencaos.veiculo_id"
+                    'table' => 'Veiculos',
+                    'type' => 'INNER',
+                    'conditions' => 'Veiculos.id = Manutencaos.veiculo_id',
+                ])
+                ->join([
+                    'table' => 'Fabricantes',
+                    'type' => 'INNER',
+                    'conditions' => 'Fabricantes.id = Veiculos.fabricante_id',
                 ])
                 ->toArray();
-        }
-        else
-        {
-            $id = $this->request->getData("id");
+        } else {
+            $id = $this->request->getData('id');
             $response = $this->Manutencaos
                 ->find()
-                ->where(["id" => $id])
+                ->where(['id' => $id])
                 ->first();
 
             // Fornecedor: CNPJ e nome
             // Veículo: Modelo, placa e nome do fabricante
         }
-
-        debug($response);
-        exit;
 
         return $this->gerarResposta($statusCode, $response);
     }
